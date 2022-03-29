2212b6356932ab3f46c4074b7b8299b922dce343
Jake Coolidge <jhcoolidge@gmail.com>
2022-03-29 10:26:14 -0700

    Add files via upload

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 try:
     from flask import Flask, render_template, request
     from bcrypt import hashpw, gensalt, checkpw
-    from google.cloud import bigquery
-    from google.oauth2 import service_account
+    from db_functions import store_user_pwd, get_user_pwd, upload_data_from_file
+    from file_handlers import *
     import re
 except ImportError as import_error:
     print("There was an error importing some packages.")
@@ -10,17 +10,8 @@ except ImportError as import_error:
     exit(-1)
 
 # -----------------------------------------------------------------------------------------
-# SETUP BIGQUERY AND FLASK APP
+# SETUP FLASK APP
 # -----------------------------------------------------------------------------------------
-
-# Set up path to bigquery credential file and load the service account from it
-key_path = 'C:\\Users\\jhcoo\\PycharmProjects\\FlaskLoginScreen\\bigquerykey.json'
-credentials = service_account.Credentials.from_service_account_file(
-    key_path, scopes=["https://www.googleapis.com/auth/cloud-platform"],
-)
-
-# Set up BQ Client with given creds
-client = bigquery.Client(credentials=credentials, project=credentials.project_id)
 
 # Set up Flask App variables
 app = Flask(
@@ -34,28 +25,64 @@ email_validate_regex = re.compile("([A-Za-z0-9]+[.-_])*[A-Za-z0-9]+@[A-Za-z0-9-]
 # and has at least one digit, lower/uppercase and a special character
 strong_password_regex = re.compile("((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*]).{6,20})")
 
+# -----------------------------------------------------------------------------------------
+# APP ROUTES
+# -----------------------------------------------------------------------------------------
 
+
+# Displays the login form
 @app.route("/")
 def index():
     return render_template("login_form.html")
 
 
+# Displays the Upload file form
+@app.route('/upload_form')
+def display_upload_form():
+    return render_template("upload_file.html")
+
+
+@app.route('/upload/file', methods=["POST"])
+def handle_uploaded_file():
+    message = ""
+
+    file = request.files["file"]
+
+    if file:
+        message = upload_data_from_file(file=file)
+    else:
+        message = "File not uploaded or table name empty."
+
+    # Re-render the template from before with the message
+    return render_template("upload_file.html", message=message)
+
+
 @app.route("/login/auth", methods=["POST"])
-def check_credentials():
+def login():
+    # Extract user inputted username and password
     username = request.form['username']
     pwd = request.form['pwd']
 
+    # Ensure the username is a valid email
     if re.fullmatch(email_validate_regex, username):
+        # Ensure the inputted password is strong enough
         if re.fullmatch(strong_password_regex, pwd):
 
+            # Retrieve the stored user password, if any
             stored_pwd_hash = get_user_pwd(username)
 
+            # If there is already a user in the DB
             if stored_pwd_hash:
+                # Check stored password hash against inputted password
                 if checkpw(pwd.encode('utf-8'), stored_pwd_hash):
                     return render_template("login_success.html")
                 message = "Invalid username or password!"
+
+            # There is not already a user in the DB
             else:
+                # Store the user's information as it fits the above policies
                 store_user_pwd(username, hashpw(pwd.encode('utf-8'), gensalt()))
+                username = ''
                 message = "New user created!"
         else:
             message = "Password is not strong enough! It has to be 8 characters long at least, " \
@@ -63,43 +90,8 @@ def check_credentials():
     else:
         message = "Invalid email address!"
 
+    # Login failed somehow, render form with message explaining why
     return render_template("login_form.html", message=message, username=username)
-
-
-def get_user_pwd(username):
-    query = "SELECT * FROM `regal-stage-343104.users.user_login` WHERE username = ? LIMIT 1;"
-    job_config = bigquery.QueryJobConfig(
-        query_parameters=[
-            bigquery.ScalarQueryParameter(None, "STRING", username)
-        ]
-    )
-
-    try:
-        result = client.query(query, job_config=job_config)
-
-        for row in result:
-            return row["password"]
-    except Exception as e:
-        print("There was an issue retrieving user's password. Error: " + str(e))
-
-    return None
-
-
-def store_user_pwd(username, hashed_pwd):
-    query = "INSERT INTO `regal-stage-343104.users.user_login` VALUES (?, ?);"
-    job_config = bigquery.QueryJobConfig(
-        query_parameters=[
-            bigquery.ScalarQueryParameter(None, "STRING", username),
-            bigquery.ScalarQueryParameter(None, "BYTES", hashed_pwd),
-        ]
-    )
-
-    try:
-        client.query(query, job_config=job_config)
-    except Exception as e:
-        print("There was a problem saving the user's password. " + str(e))
-        return False
-    return True
 
 
 if __name__ == '__main__':
